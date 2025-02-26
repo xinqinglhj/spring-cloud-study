6b13f0fd196965d3feeeb67a81cbc2da5efb1dab
nacycher <nacycher@163.com>
2025-02-26 16:25:47 +0800

    [add]13-配置中心

--- services/service-order/src/main/java/com/order/controller/OrderController.java
+++ services/service-order/src/main/java/com/order/controller/OrderController.java
@@ -1,18 +1,36 @@
 package com.order.controller;
 
 import com.order.Order;
+import com.order.properties.OrderProperties;
 import com.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+//@RefreshScope // 激活配置自动刷新功能
 @RestController
 @RequestMapping("/order")
 public class OrderController {
     @Autowired
     private OrderService orderService;
+    @Autowired
+    private OrderProperties orderProperties;
 
+//    // 获取Nacos中的配置
+//    @Value("${order.timeout}")
+//    String orderTimeout;
+//    @Value("${order.auto-confirm}")
+//    String orderAutoConfirm;
+
+
+    @GetMapping("/getConfig")
+    public String getConfig() {
+        return "orderTimeout: " + orderProperties.getTimeout() + ", orderAutoConfirm: " + orderProperties.getAutoConfirm();
+    }
 
     /**
      * 获取订单
---
+++ services/service-order/src/main/java/com/order/properties/OrderProperties.java
@@ -0,0 +1,20 @@
+package com.order.properties;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * 使用@ConfigurationProperties注解来批量读取配置
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "order")
+public class OrderProperties {
+
+    String timeout;
+    String autoConfirm;
+}
