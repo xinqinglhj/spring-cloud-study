c8798814aae29e67c0445f14f4529ab8a78ab8a2
nacycher <nacycher@163.com>
2025-02-27 11:29:28 +0800

    [add]14-nacosConfigManager动态监听配置变化

--- services/service-order/src/main/java/com/order/OrderMainApplication.java
+++ services/service-order/src/main/java/com/order/OrderMainApplication.java
@@ -1,7 +1,16 @@
 package com.order;
 
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 
 @SpringBootApplication
@@ -9,5 +18,33 @@ public class OrderMainApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderMainApplication.class, args);
+	}
+
+	/**
+	 * 使用nacosConfigManager监听配置的变化：
+	 * 	1.项目启动，监听配置文件
+	 * 	2.项目发生变化后拿到值
+	 * 	3.发送邮件
+	 * @param nacosConfigManager
+	 * @return
+	 */
+	@Bean
+	ApplicationRunner applicationRunner(NacosConfigManager nacosConfigManager) {
+		return args -> {
+			ConfigService configService = nacosConfigManager.getConfigService();
+			configService.addListener("server-order.properties", "DEFAULT_GROUP", new Listener() {
+				@Override
+				public Executor getExecutor() {
+					return Executors.newFixedThreadPool(4);
+				}
+
+				@Override
+				public void receiveConfigInfo(String s) {
+					System.out.println("变化的配置：" + s);
+					System.out.println("邮件通知配置变化");
+				}
+			});
+			System.out.println("============nacosConfigManager监听配置============");
+		};
 	}
 }
